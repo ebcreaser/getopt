1c0b3dcfc9976bb8f8d36f9b28995ef15901f332
Ethan Creaser <ebcreaser@proton.me>
2022-11-28 12:43:54 -0500

    refactored

--- getopt.c
+++ getopt.c
@@ -2,6 +2,9 @@
 #include <string.h>
 #include "getopt.h"
 
+static int _handle_opt(int argc, char *const argv[],
+		const char *opt_ptr, int *nextchar_ptr);
+
 char *optarg = NULL;
 int optind = 1;
 
@@ -9,83 +12,77 @@ int
 getopt(int argc, char *const argv[], 
 		const char *optstring) 
 {
+	char *opt_ptr;
 	static int nextchar = 0;
 	int opt = 0;
-	int i;
 
-	while (opt == 0) {
-		if (optind == argc) {
-			opt = -1;
-			break;
-		}
-		switch (argv[optind][nextchar]) {
-		case '\0':
-			++optind;
-			nextchar = 0;
-			break;
-		case '-':
-			if (nextchar == 0) {
-				++nextchar;
-			} else {
-				++optind;
-				nextchar = 0;
+	if (optind < argc && argv[optind][nextchar] == '\0') {
+		++optind;
+		nextchar = 0;
+	}
+	if (nextchar == 0) {
+		for (;; ++optind) {
+			if (optind >= argc) {
+				return -1;
 			}
-			break;
-		case ':':
-		case ';':
-			opt = '?';
-			++nextchar;
-			break;
-		default:
-			if (nextchar == 0) {
-				opt = -1;
-				break;
-			}
-			optarg = NULL;
-			for (i = 0; opt == 0; ++i) {
-				if (optstring[i] == '\0') {
-					opt = '?';
-					++nextchar;
-					break;
-				}
-				if (argv[optind][nextchar] != optstring[i]) {
+			if (argv[optind][0] == '-') {
+				if (argv[optind][1] == '-') {
 					continue;
 				}
-				opt = argv[optind][nextchar];
-				++nextchar;
-				if (optstring[i + 1] != ':') {
-					break;
-				}
-				switch (argv[optind][nextchar]) {
-				case '\0':
-					if (optind + 1 < argc && argv[optind + 1][0] != '-') {
-						optarg = argv[optind + 1];
-						optind += 2;
-						nextchar = 0;
-					} else if (optstring[i + 2] != ':' ) {
-						opt = '?';
-					}
-					break;
-				case '=':
-					++nextchar;
-					if (argv[optind][nextchar] != '\0') {
-						optarg = argv[optind] + nextchar;
-					} else if (optstring[i + 2] != ':' ) {
-						opt = '?';
-					}
-					++optind;
-					nextchar = 0;
-					break;
-				default:
-					optarg = argv[optind] + nextchar;
-					++optind;
-					nextchar = 0;
-					break;
-				}
+				break;
+			}
+		}
+		++nextchar;
+	}
+	opt_ptr = strchr(optstring, argv[optind][nextchar]);
+	opt = _handle_opt(argc, argv, opt_ptr, &nextchar);
+
+	return opt;
+}
+
+static int
+_handle_opt(int argc, char *const argv[],
+		const char *opt_ptr, int *nextchar_ptr)
+{
+	int opt = 0;
+
+	optarg = NULL;
+	if (opt_ptr == NULL) {
+		++(*nextchar_ptr);
+		return '?';
+	}
+	if (opt_ptr[1] != ':') {
+		++(*nextchar_ptr);
+		return *opt_ptr;
+	}
+	switch (argv[optind][*nextchar_ptr + 1]) {
+	case '\0':
+		if (optind + 1 >= argc || argv[optind + 1][0] == '-') {
+			if (opt_ptr[2] != ':') {
+				opt = '?';
 			}
 			break;
 		}
+		optarg = argv[optind + 1];
+		break;
+	case '=':
+		if (argv[optind][*nextchar_ptr + 2] == '\0') {
+			if (opt_ptr[2] != ':') {
+				opt = '?';
+			}
+			break;
+		}
+		optarg = argv[optind] + *nextchar_ptr + 2;
+		break;
+	default:
+		optarg = argv[optind] + *nextchar_ptr + 1;
+		break;
 	}
+	if (opt != '?') {
+		opt = *opt_ptr;
+	}
+	++optind;
+	*nextchar_ptr = 0;
 
 	return opt;
 }
@@ -94,130 +91,7 @@ int getopt_long(int argc, char *const argv[],
 		const char *optstring,
 		const struct option *longopts, int *longindex)
 {
-	static int nextchar = 0;
 	int opt = 0;
-	int i;
-	int cont = 1;
-
-	while (cont) {
-		if (optind == argc) {
-			opt = -1;
-			break;
-		}
-		switch (argv[optind][nextchar]) {
-		case '\0':
-			++optind;
-			nextchar = 0;
-			break;
-		case '-':
-			if (nextchar != 0) {
-				++nextchar;
-				opt = '?';
-			}
-			++nextchar;
-			if (argv[optind][nextchar] != '-') {
-				break;
-			}
-			++nextchar;
-			for (i = 0;; ++i) {
-				if (longopts[i].name == NULL) {
-					opt = '?';
-					break;
-				}
-				if (strcmp(argv[optind] + nextchar, longopts[i].name) != 0) {
-					continue;
-				}
-				opt = longopts[i].val;
-				*longindex = i;
-				if (longopts[i].has_arg == no_argument) {
-					break;
-				}
-				do {
-					++nextchar;
-				} while (argv[optind][nextchar] != '=' && argv[optind][nextchar] != '\0');
-				switch (argv[optind][nextchar]) {
-				case '=':
-					++nextchar;
-					if (argv[optind][nextchar] == '\0' && longopts[i].has_arg == required_argument) {
-						opt = '?';
-					} else {
-						optarg = argv[optind] + nextchar;
-					}
-					break;
-				case '\0':
-					if ((optind + 1 >= argc || argv[optind + 1][0] == '-') 
-							&& longopts[i].has_arg == required_argument) {
-						opt = '?';
-					} else {
-						optarg = argv[optind + 1];
-					}
-					break;
-				}
-				break;
-			}
-			++optind;
-			nextchar = 0;
-			cont = 0;
-			break;
-		case ':':
-		case ';':
-			opt = '?';
-			++nextchar;
-			break;
-		default:
-			if (nextchar == 0) {
-				++optind;
-				break;
-			}
-			optarg = NULL;
-			for (i = 0; opt == 0; ++i) {
-				if (optstring[i] == '\0') {
-					opt = '?';
-					++nextchar;
-					break;
-				}
-				if (argv[optind][nextchar] != optstring[i]) {
-					continue;
-				}
-				opt = argv[optind][nextchar];
-				++nextchar;
-				if (optstring[i + 1] != ':') {
-					break;
-				}
-				switch (argv[optind][nextchar]) {
-				case '\0':
-					if (optind + 1 < argc && argv[optind + 1][0] != '-') {
-						optarg = argv[optind + 1];
-						optind += 2;
-						nextchar = 0;
-					} else if (optstring[i + 2] != ':' ) {
-						opt = '?';
-					}
-					break;
-				case '=':
-					++nextchar;
-					if (argv[optind][nextchar] != '\0') {
-						optarg = argv[optind] + nextchar;
-					} else if (optstring[i + 2] != ':' ) {
-						opt = '?';
-					}
-					++optind;
-					nextchar = 0;
-					break;
-				default:
-					optarg = argv[optind] + nextchar;
-					++optind;
-					nextchar = 0;
-					break;
-				}
-			}
-			cont = 0;
-			break;
-		}
-		if (opt == '?') {
-			break;
-		}
-	}
 
 	return opt;
 }
--- main.c
+++ main.c
@@ -4,8 +4,7 @@
 int main(int argc, char *argv[]) {
 	const char *optstring = "ab::c:";
 	int opt = 0;
-	int longindex = 0;
-	struct option long_options[] = {
+	/* struct option long_options[] = {
 		{"add",     required_argument, 0,  0 },
 		{"append",  no_argument,       0,  0 },
 		{"delete",  required_argument, 0,  0 },
@@ -13,15 +12,15 @@ int main(int argc, char *argv[]) {
 		{"create",  required_argument, 0, 'c'},
 		{"file",    required_argument, 0,  0 },
 		{0,         0,                 0,  0 }
-    };
+    }; */
 
-	opt = getopt_long(argc, argv, optstring, long_options, &longindex);
+	opt = getopt(argc, argv, optstring);
 	while (opt != -1) {
 		printf("%c\n", opt);
 		if (optarg != NULL) {
 			printf("\t%s\n", optarg);
 		}
-		opt = getopt_long(argc, argv, optstring, long_options, &longindex);
+		opt = getopt(argc, argv, optstring);
 	}
 
 	return 0;
